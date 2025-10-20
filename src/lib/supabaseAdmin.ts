d18ca15b199f2c8bf7d4027a28d95678eac2b478
zahirsnyder <67769713+zahirsnyder@users.noreply.github.com>
2025-10-20 21:42:58 +0800

    FIX

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const serviceKey = process.env.SUPABASE_SERVICE_ROLE!; // 🚫 Never expose this to client
+const serviceKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!; // 🚫 Never expose this to client
 
 export const supabaseAdmin = createClient(supabaseUrl, serviceKey, {
   auth: { persistSession: false },
