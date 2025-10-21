cd67a3b21eba83e0074ebc6bd392e84da158d8dd
zahirsnyder <67769713+zahirsnyder@users.noreply.github.com>
2025-10-21 11:29:02 +0800

    FIX

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: supabaseUrl.replace("https://", ""),
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
